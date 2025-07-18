2ecd58bbd2a1163fe97e24bb94ced322e3afad6f
Spencer Teillon at doco <teillo01@email.franklin.edu>
2025-07-18 13:52:51 -0400

    up to 4.7

--- src/download.js
+++ src/download.js
@@ -1,22 +1,29 @@
 import net from 'net';
 import { Buffer } from 'buffer';
 import { getPeers } from './tracker.js';
+import * as message from './message.js';
 
 export default function (torrent) {
     getPeers(torrent, peers => {
-        peers.forEach(download);
+        peers.forEach(peer => download(peer, torrent));
     })
 }
 
-function download(peer) {
+function download(peer, torrent) {
     const socket = new net.Socket();
     socket.on('error', console.log);
     socket.connect(peer.port, peer.ip, () => {
-        // socket.write() todo   
+        socket.write(message.buildHandshake(torrent));
     });
-    onWholeMsg(socket, data => {
-        // handle response
-    })
+    onWholeMsg(socket, msg => msgHandler(msg, socket));
+}
+
+function msgHandler(msg, socket) {
+    if (isHandshake(msg)) socket.write(message.buildInterested());
+}
+
+function isHandshake(msg) {
+    return msg.length === msg.readUInt8(0) + 49  && msg.toString('utf8', 1) === 'BitTorrent Protocol';
 }
 
 function onWholeMsg(socket, callback) {
