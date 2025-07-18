0a5a95f504dab9853dd6af0585941efad01eddc4
Spencer Teillon at doco <teillo01@email.franklin.edu>
2025-07-18 13:40:27 -0400

    up to 4.6

--- src/download.js
+++ src/download.js
@@ -12,9 +12,26 @@ function download(peer) {
     const socket = new net.Socket();
     socket.on('error', console.log);
     socket.connect(peer.port, peer.ip, () => {
-        //socket.write() todo   
+        // socket.write() todo   
     });
-    socket.on('data', data => {
-        //handle response
+    onWholeMsg(socket, data => {
+        // handle response
+    })
+}
+
+function onWholeMsg(socket, callback) {
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuf => {
+        //calc length of whole message
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+        while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+            callback(savedBuf.slice(0, msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
     })
 }
